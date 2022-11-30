08988cac58d13549550e13ce6e242a3230efea0f
MrJeyhun <ceyhun.zade1998@gmail.com>
2022-12-01 00:13:52 +0400

    added examples about promise race, all, allSettled, any

--- promises.js
+++ promises.js
@@ -72,3 +72,148 @@ const wait2 = (
 
 //   Promise.all() takes an array (or any iterable) and returns a promise that resolves when all of the promises in the iterable argument have resolved, 
 //   or rejects with the reason of the first passed promise that rejects.
+
+
+// 1. Need a way to pass arguments to callback and return result without actually returning in object creation paramter.
+// 2. Here (arg) => {this.values.resolved = arg} is not callback, but an argument passed to callback
+function Prom(callback) {
+    this.values = {resolved: null, rejected:null}
+    const resolve = (arg) => {this.values.resolved = arg}
+    const reject = (arg) => {this.values.resolved = arg}
+    this.then = () => {
+        callback(resolve, reject);
+        return  new CustomPromise(this.values);
+    }
+ }
+ 
+ const a = new Prom((resolve) => { 
+     resolve(" HELLOOOO!")
+ })
+
+ console.log(a.then());
+
+
+
+
+//  Here’s a simple code example where the Promise.all method consumes getFrogs and getLizards, 
+//  which are promises. It retrieves the results as an array inside the .then handler before storing them into the local storage
+ const getFrogs = new Promise((resolve) => {
+    resolve([
+      { id: 'mlo29naz', name: 'larry', born: '2016-02-22' },
+      { id: 'lp2qmsmw', name: 'sally', born: '2018-09-13' },
+    ])
+  })
+  
+  const getLizards = new Promise((resolve) => {
+    resolve([
+      { id: 'aom39d', name: 'john', born: '2017-08-11' },
+      { id: '20fja93', name: 'chris', born: '2017-01-30' },
+    ])
+  })
+  
+  function addToStorage(item) {
+    if (item) {
+      let prevItems = localStorage.getItem('items')
+      
+      if (typeof prevItems === 'string') {
+        prevItems = JSON.parse(prevItems)
+      } else {
+        prevItems = []
+      }
+      
+      const newItems = [...prevItems, item]
+      localStorage.setItem('items', JSON.stringify(newItems))
+    }
+  }
+  
+  let allItems = []
+  
+  Promise.all([getFrogs, getLizards])
+    .then(([frogs, lizards]) => {
+      localStorage.clear()
+      
+      frogs.forEach((frog) => {
+        allItems.push(frog)
+      })
+      
+      lizards.forEach((lizard) => {
+        allItems.push(lizard)
+      })
+      
+      allItems.forEach((item) => {
+        addToStorage(item)
+      })
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+    
+  console.log(localStorage.getItem('items'))
+  /*
+    result:
+      [{"id":"mlo29naz","name":"larry","born":"2016-02-22"},{"id":"lp2qmsmw","name":"sally","born":"2018-09-13"},{"id":"aom39d","name":"john","born":"2017-08-11"},{"id":"20fja93","name":"chris","born":"2017-01-30"}]
+  */
+   
+//Promise.allSettled
+const add = (num1, num2) => new Promise((resolve) => resolve(num1 + num2))
+
+const multiply1 = (num1, num2) => new Promise((resolve) => resolve(num1 * num2))
+
+const fail1 = (num1) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => reject(new Error('You, my friend, were too late')), 200),
+  )
+  
+const fail2 = (num1) =>
+  new Promise((resolve, reject) =>
+    setTimeout(
+      () => reject(new Error('Being late is never a good habit')),
+      100,
+    ),
+  )
+  
+const promises1 = [add(2, 4), multiply1(5, 5), fail1('hi'), fail2('hello')]
+
+Promise.allSettled(promises)
+  .then((result) => {
+    console.log(result)
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
+
+//Promise.any
+const multiply = (num1, num2) => new Promise((resolve) => resolve(num1 * num2))
+
+const fail = (num1) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => reject(new Error('You, my friend, were too late')), 200),
+  )
+  
+const promises2 = [
+  fail(2),
+  fail(),
+  fail(),
+  multiply(2, 2),
+  fail(2),
+  fail(2),
+  fail(2, 2),
+  fail(29892),
+  fail(2),
+  fail(2, 2),
+  fail('hello'),
+  fail(2),
+  fail(2),
+  fail(1),
+  fail(),
+]
+
+Promise.any(promises2)
+  .then((result) => {
+    console.log(result) // result: 4
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+
