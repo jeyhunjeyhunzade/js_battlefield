27ab3aca65e9c8314eee43e975b5161d2eb6c529
MrJeyhun <ceyhun.zade1998@gmail.com>
2022-12-05 21:45:26 +0400

    updated promises with promise finally method & added call bind apply examples

---
+++ call_bind_applly.js
@@ -0,0 +1,16 @@
+//With the bind() method, an object can borrow a method from another object.
+const person = {
+    firstName:"John",
+    lastName: "Doe",
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  
+  const member = {
+    firstName:"Hege",
+    lastName: "Nilsen",
+  }
+  
+let fullName = person.fullName.bind(member);
+console.log("fullName", fullName())
--- promises.js
+++ promises.js
@@ -217,3 +217,26 @@ Promise.any(promises2)
     console.error(error)
   })
 
+
+
+  // Promise.finally() - This lets you avoid duplicating code in both the promise's then() and catch() handlers.
+  function checkMail() {
+    return new Promise((resolve, reject) => {
+      if (Math.random() > 0.5) {
+        resolve('Mail has arrived');
+      } else {
+        reject(new Error('Failed to arrive'));
+      }
+    });
+  }
+  
+  checkMail()
+    .then((mail) => {
+      console.log(mail);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+    .finally(() => {
+      console.log('Experiment completed');
+    });
