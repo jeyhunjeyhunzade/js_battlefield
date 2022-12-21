e784e89cf584f545bde3f90f96dd8d5fdc11209b
Jeyhun Jeyhunzada <46054708+MrJeyhun@users.noreply.github.com>
2022-12-22 01:15:19 +0400

    Updated ts notes

--- ts_notes.ts
+++ ts_notes.ts
@@ -97,3 +97,81 @@ type Reponse = [string, number]
 interface Response {
   value: [string, number]
 }
+
+
+// Generics  (for reusable components)
+// without generics:
+function identity(arg: any): any {
+  return arg;
+}
+
+// with generics:
+function identity<T>(arg: T): T {
+  return arg;
+} // identity function is generic now, because we can use it with whatever type we want
+
+// Array of Type
+function loggingIdentity<Type>(arg: Type[]): Type[] {
+  console.log(arg.length);
+  return arg;
+}
+
+// Awaited<Type>
+type A = Awaited<Promise<string>>;
+// type A = string
+ 
+type B = Awaited<Promise<Promise<number>>>;
+// type B = number
+ 
+type C = Awaited<boolean | Promise<number>>;
+// type C = number | boolean
+
+
+//Partial<Type>
+interface Todo {
+  title: string;
+  description: string;
+}
+ 
+function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+  return { ...todo, ...fieldsToUpdate };
+}
+ 
+const todo1 = {
+  title: "organize desk",
+  description: "clear clutter",
+};
+ 
+const todo2 = updateTodo(todo1, {
+  description: "throw out trash",
+});
+
+
+//Omit<Type, Keys>
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+  createdAt: number;
+}
+ 
+type TodoPreview = Omit<Todo, "description">;
+ 
+const todo: TodoPreview = {
+  title: "Clean room",
+  completed: false,
+  createdAt: 1615544252770,
+};
+ 
+todo;
+ 
+const todo: TodoPreview
+ 
+type TodoInfo = Omit<Todo, "completed" | "createdAt">;
+ 
+const todoInfo: TodoInfo = {
+  title: "Pick up kids",
+  description: "Kindergarten closes at 5pm",
+};
+ 
+todoInfo;
