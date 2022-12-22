e0ed2cad166b9832ace154eaaf5c77e75ea6647b
Jeyhun Jeyhunzada <46054708+MrJeyhun@users.noreply.github.com>
2022-12-22 13:53:46 +0400

    added react memo test case

---
+++ react_memo.js
@@ -0,0 +1,30 @@
+import React, { Component, useState, memo, useEffect, useMemo } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const colors = ['red', 'green'];
+
+  const callbackFucn = useMemo(() => colors, []);
+
+  const increase = () => setCount(count + 1);
+
+  return (
+    <>
+      <button onClick={increase}>Increase</button>
+      <div>{count}</div>
+      <ChildComp callbackFucn={callbackFucn} />
+    </>
+  );
+};
+
+const ChildComp = memo(({ callbackFucn }) => {
+  const [colorProp, setColor] = useState();
+
+  console.log('updated');
+  return <div>{colorProp}</div>;
+});
+
+render(<App appName="User Details" />, document.getElementById('root'));
