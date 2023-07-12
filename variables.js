254d3523436a321cac2e931a965919ef8958d4e1
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-07-12 20:12:42 +0300

    updated variables js

--- variables.js
+++ variables.js
@@ -62,3 +62,60 @@ parseInt("123blue"); //123
 toString(null) | toString(undefined); // they are not available
 String(undefined); // 'undefined'
 String(null); // 'null'
+
+// OPERATORS
+
+// The prefix increment and decrement are equal in terms of order of
+// precedence in a statement and are therefore evaluated left to right. Consider this example:
+let num11 = 2;
+let num22 = 20;
+let num33 = --num1 + num2;
+let num44 = num1 + num2;
+console.log(num33); // 21 console.log(num4);
+console.log(num44); // 21
+
+// The postfix versions of increment and decrement use the same syntax (++ and --, respectively)
+//  but are placed after the variable instead of before it.
+let num1 = 2;
+let num2 = 20;
+let num3 = num1-- + num2;
+let num4 = num1 + num2;
+console.log(num3); // 22 console.log(num4); // 21
+console.log(num4); // 21
+
+//Booleans
+let found = true;
+let result = found && someUndeclaredVariable; // error occurs here console.log(result);
+console.log(result); // this line never executes
+
+let found2 = false;
+let result2 = found2 && someUndeclaredVariable; // no error console.log(result);
+console.log(result2); // works, because variable some- UndeclaredVariable is undefined, it is never evaluated because the first operand is false.
+
+let found3 = true;
+let result3 = found3 || someUndeclaredVariable; // no error
+console.log(result3); // works
+
+let found4 = true;
+let result4 = found4 || someUndeclaredVariable; // error occurs here console.log(result);
+console.log(result4); // this line never executes
+
+//Equality operators
+undefined == 0; // false
+null == 0; // false
+true == 2; // false
+
+// with Statement
+
+// witout "with"
+let qs = location.search.substring(1);
+let hostName = location.hostname;
+let url = location.href;
+
+// with "with"
+with (location) {
+  let qs = search.substring(1);
+  let hostName = hostname;
+  let url = href;
+}
+//In strict mode, the with statement is not allowed and is considered a syntax error.
