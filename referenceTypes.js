e0db91323e517322bef108f416ba41035328beaf
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-07-19 20:14:41 +0300

    updated referenceTypes js

--- referenceTypes.js
+++ referenceTypes.js
@@ -6,3 +6,13 @@ const arrayLikeObject = {
   length: 4,
 };
 alert(Array.from(arrayLikeObject)); // [1, 2, 3, 4]
+
+// map() will skip the holes entirely
+alert(options.map(() => 6)); // [6, undefined, undefined, undefined, 6]
+
+// join() treats holes as empty strings
+alert(options.join("-")); // "1----5"
+
+let colors = ["red", "blue", "green"]; // creates an array with three strings
+colors.length = 2;
+alert(colors[2]); // undefined
