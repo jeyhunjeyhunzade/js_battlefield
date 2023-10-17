220a0d4221969e5bc6f8f550537b3e9fe9ec0acc
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-17 19:21:40 +0300

    added async defer examples

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -1,25 +1,24 @@
-const longestCommonPrefix = function(strs) {
-    let output = ''
+const longestCommonPrefix = function (strs) {
+  let output = "";
 
-    for (let y = 0; y < strs[0].length; y++) {
-        // if (!strs[0].length) return ""
-        let samePrefx;
-        for (i = 0; i < strs.length; i++) {
-            if (!strs[i].length) return ""
-            if (strs[0][y] == strs[i][y]) {
-                samePrefx = strs[0][y]
-            } else {
-                return output
-            }
-        }
-        output += samePrefx
+  for (let y = 0; y < strs[0].length; y++) {
+    // if (!strs[0].length) return ""
+    let samePrefx;
+    for (i = 0; i < strs.length; i++) {
+      if (!strs[i].length) return "";
+      if (strs[0][y] == strs[i][y]) {
+        samePrefx = strs[0][y];
+      } else {
+        return output;
+      }
     }
-    return output
-    
+    output += samePrefx;
+  }
+  return output;
 };
 
-let testArr = ["flower","","flower","flowe"]
+let testArr = ["flower", "", "flower", "flowe"];
 
-console.log(longestCommonPrefix(testArr))
+console.log(longestCommonPrefix(testArr));
 
 // exceptional case , if array has empty string
