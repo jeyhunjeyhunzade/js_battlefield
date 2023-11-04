465a749a2245fb5479b8cfc2b3f52d079e670003
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-11-04 21:59:09 +0300

    updated array methods, reference types & call bind apply js

--- part1/call_bind_applly.js
+++ part1/call_bind_applly.js
@@ -1,16 +1,33 @@
 //With the bind() method, an object can borrow a method from another object.
 const person = {
-    firstName:"John",
-    lastName: "Doe",
-    fullName: function () {
-      return this.firstName + " " + this.lastName;
-    }
-  }
-  
+  firstName: "John",
+  lastName: "Doe",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
 const member = {
-  firstName:"Hege",
+  firstName: "Hege",
   lastName: "Nilsen",
-}
-  
+};
+
 let fullName = person.fullName.bind(member);
-console.log("fullName", fullName())
+console.log("fullName", fullName());
+
+// ----------------------------------------------------------------
+var Button = function (content) {
+  this.content = content;
+};
+Button.prototype.click = function () {
+  console.log(this.content + " clicked");
+};
+
+var myButton = new Button("OK");
+myButton.click(); // OK clicked
+
+var looseClick = myButton.click;
+looseClick(); // not bound, 'this' is not myButton - it is the global object: // undefined clicked
+
+var boundClick = myButton.click.bind(myButton);
+boundClick(); // bound, 'this' is myButton: // OK clicked
