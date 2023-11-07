b000f5e5fddd418f4b321c8065a48adb855931e8
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-11-07 19:54:05 +0300

    updated pre/post fix

--- part2/prefix_postfix.js
+++ part2/prefix_postfix.js
@@ -21,5 +21,6 @@
   let num2 = 20;
   let num3 = num1-- + num2;
   let num4 = num1 + num2;
-  console.log(num3); // 22 console.log(num4); // 21
+  console.log(num3); // 22
+  console.log(num4); // 21
 }
