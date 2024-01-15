294fe0e386df1372c0b298dc17dd7677755831f0
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2024-01-15 18:41:54 +0300

    added pushNotification js

--- gist.js
+++ gist.js
@@ -1,14 +1,2 @@
-{const foo = "foo";
-
-// Behaves identically to "export default foo;"
-export { foo as default };}
-
-{
-
-    // Variable declarations cannot occur inside inline default exports e
-    export default const foo = 'bar';
-    // Only identifiers can appear in export clauses 
-    export { 123 as foo }'
-    // Aliasing only can occur in export clauses 
-    export const foo = 'foo' as myFoo;
-}
+// loose coupling of CSS to JavaScript
+element.className = "edit";
