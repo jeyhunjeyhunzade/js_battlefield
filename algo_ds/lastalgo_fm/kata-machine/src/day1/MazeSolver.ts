9988b4a410b0c7398750677b9712870e683d86d3
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2024-01-21 01:50:47 +0300

    solved MazeSolver

--- algo_ds/lastalgo_fm/kata-machine/src/day1/MazeSolver.ts
+++ algo_ds/lastalgo_fm/kata-machine/src/day1/MazeSolver.ts
@@ -1,6 +1,92 @@
+// dir: [x, y][]
+const dir = [
+    [0, 1], // top,
+    [1, 0], // right
+    [0, -1], // bottom
+    [-1, 0], // left
+];
+
+function walk(
+    maze: string[],
+    wall: string,
+    curr: Point,
+    end: Point,
+    seen: boolean[][],
+    path: Point[],
+): boolean {
+    // 1. Base Cases
+
+    // off the map
+    if (
+        curr.x < 0 ||
+        curr.x >= maze[0].length ||
+        curr.y < 0 ||
+        curr.y >= maze.length
+    ) {
+        return false;
+    }
+
+    // on the wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    // have seen it
+    if (seen[curr.y][curr.x]) {
+        return false;
+    }
+
+    // reached the end
+    if (curr.x === end.x && curr.y === end.y) {
+        path.push(end);
+        return true;
+    }
+
+    // 2. Recurse Case
+
+    // pre
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
+
+    // recurse
+    for (let i = 0; i < dir.length; i++) {
+        const [x, y] = dir[i];
+
+        if (
+            walk(
+                maze,
+                wall,
+                {
+                    x: curr.x + x,
+                    y: curr.y + y,
+                },
+                end,
+                seen,
+                path,
+            )
+        ) {
+            return true;
+        }
+    }
+
+    // post
+    path.pop();
+    return false;
+}
 export default function solve(
     maze: string[],
     wall: string,
     start: Point,
     end: Point,
-): Point[] {}
+): Point[] {
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
+
+    for (let i = 0; i < maze.length; i++) {
+        seen.push(new Array(maze[0].length).fill(false));
+    }
+
+    walk(maze, wall, start, end, seen, path);
+
+    return path;
+}
